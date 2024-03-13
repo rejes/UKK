41957dc9ea1366eebcd836e24e9893fe83e4170f
rejes <rejes122@gmail.com>
2024-03-13 10:56:28 +0800

    Frontend

---
+++ ukk-laravel/resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- css bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- our css -->
+    <link rel="stylesheet" href="css/dashboard.css">
+</head>
+<body>
+<nav class="bg-success">
+<div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="/dashboard" class="nav-link text-white" aria-current="page">Dashboard</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Tabel Data</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">Create Data</a></li>
+        <!-- <li class="nav-item"><a href="#" class="nav-link text-white">FAQs</a></li> -->
+        <li class="nav-item"><a href="#" class="nav-link text-white">About Me</a></li>
+      </ul>
+    </header>
+  </div>
+</nav>
+
+<!-- js bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
